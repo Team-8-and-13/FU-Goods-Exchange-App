95d071bc5e603897cdcb8911bd2ee316ba0b8bca
Lê Hồ Hoàng Linh <135522356+HoangLinhLe@users.noreply.github.com>
2024-09-30 00:04:55 +0700

    CRUD

---
+++ FU Good Exchange App/FUExchange.Contract.Repositories/IUOW/IBanRepository.cs
@@ -0,0 +1,14 @@
+﻿using FUExchange.Contract.Repositories.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUExchange.Contract.Repositories.IUOW
+{
+    internal interface IBanRepository
+    {
+        Task<IEnumerable<Ban>> GetAllReportAsync(string reportId);
+    }
+}
---
+++ FU Good Exchange App/FUExchange.ModelViews/BanModelViews/BanModelView.cs
@@ -0,0 +1,9 @@
+﻿
+namespace FUExchange.ModelViews.BanModelViews
+{
+    public class BanModelView
+    {
+        required public string? ReportId { get; set; }
+        required public DateTime? Expires { get; set; }
+    }
+}
