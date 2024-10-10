337197f33ddf2f2875d01fea6a81b134759623c6
___nCti3n <tn216003@gmail.com>
2024-10-10 18:08:25 +0700

    fix thuoc tinh User

--- FU Good Exchange App/FUExchange.ModelViews/UserModelViews/UpdateUserModel.cs
+++ FU Good Exchange App/FUExchange.ModelViews/UserModelViews/UpdateUserModel.cs
@@ -8,10 +8,10 @@ namespace FUExchange.ModelViews.UserModelViews
 {
     public class UpdateUserModel
     {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
-        public string FullName { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
+        public required string FullName { get; set; }
     }
 }
--- FU Good Exchange App/FUExchange.ModelViews/UserModelViews/UserResponseModel.cs
+++ FU Good Exchange App/FUExchange.ModelViews/UserModelViews/UserResponseModel.cs
@@ -3,7 +3,7 @@
     public class UserResponseModel
     {
         public string? Id { get; set; }
-        public string Email { get; set; }
+        public required string Email { get; set; }
         public string Username { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public IList<string> Roles { get; set; } = new List<string>();
