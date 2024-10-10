f895cd8fad54564a82d1a42ff0700677b2f40fba
khoatranBao <khoatran2272002@gmail.com>
2024-10-10 13:03:06 +0700

    exchange_Khoa

--- FU Good Exchange App/FUExchange.Services/Service/ExchangeService.cs
+++ FU Good Exchange App/FUExchange.Services/Service/ExchangeService.cs
@@ -2,45 +2,105 @@
 using FUExchange.Contract.Repositories.Interface;
 using FUExchange.Contract.Services.Interface;
 using FUExchange.Core;
+using FUExchange.Core.Constants;
+using FUExchange.Core.Utils;
+using FUExchange.ModelViews.ExchangeModelViews;
+using FUExchange.ModelViews.ProductModelViews;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using static FUExchange.Core.Base.BaseException;
+using FUExchange.ModelViews.CategoryModelViews;
 
 namespace FUExchange.Services.Service
 {
     public class ExchangeService : IExchangeService
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ExchangeService(IUnitOfWork unitOfWork)
+        private readonly IMapper _mapper;
+        public ExchangeService(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
-        public async Task CreateExchangeAsync(Exchange exc, string userId)
+        public async Task CreateExchangeAsync(CreateExchangeModelViews exc)
         {
-            exc.CreatedBy = userId;
-            exc.CreatedTime = DateTime.Now;
-            await _unitOfWork.GetRepository<Exchange>().InsertAsync(exc);
+            if (exc == null)
+            {
+                throw new ErrorException(StatusCodes.Status400BadRequest, ResponseCodeConstants.BADREQUEST, ErrorMessages.ERROR);
+            }
+
+            IHttpContextAccessor httpContext = new HttpContextAccessor();
+            var User = httpContext.HttpContext?.User;
+
+            var Exc = new Exchange
+            {
+                ProductId = exc.ProductId,
+                BuyerId = new Guid(User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value),
+                CreatedBy = User.Identity?.Name,
+                CreatedTime = DateTime.Now
+            };
+
+            await _unitOfWork.GetRepository<Exchange>().InsertAsync(Exc);
             await _unitOfWork.SaveAsync();
         }
 
-        public async Task<Exchange> DeleteExchangeAsync(string id, string userId)
+        public async Task DeleteExchangeAsync(string id)
+        {
+            var EXC = await _unitOfWork.GetRepository<Exchange>().GetByIdAsync(id);
+            if (EXC == null)
+            {
+                throw new ErrorException(StatusCodes.Status404NotFound, ResponseCodeConstants.NOT_FOUND, "Không tìm thấy Trao đổi");
+            }
+            IHttpContextAccessor httpContext = new HttpContextAccessor();
+            var User = httpContext.HttpContext?.User;
+            var userName = User.Identity.Name;
+            Guid userId = new Guid(User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value);
+            if (EXC.BuyerId != userId)
+            {
+                throw new ErrorException(StatusCodes.Status401Unauthorized, ResponseCodeConstants.UNAUTHORIZED, "Unauthorized !!!");
+            }
+            else
+            {
+                EXC.DeletedBy = userName;
+                await _unitOfWork.GetRepository<Exchange>().UpdateAsync(EXC);
+                await _unitOfWork.SaveAsync();
+            }
+        }
+
+        public async Task<BasePaginatedList<ExchangeModelViews>> GetAllExchangeAsync(int pageIndex, int pageSize)
+        {
+
+            var query = _unitOfWork.GetRepository<Exchange>().Entities.Where(p => !p.DeletedTime.HasValue);
+            var totalCount = await query.CountAsync();
+            var paginatedItems = await query
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            var mappedItems = paginatedItems.Select(exchange => new ExchangeModelViews
+            {
+                BuyerId = exchange.BuyerId.Value.ToString(),
+                ProductId = exchange.ProductId
+            }).ToList();
+            return new BasePaginatedList<ExchangeModelViews>(mappedItems, totalCount, pageIndex, pageSize);
+        }
+
+        public async Task<ExchangeModelViews?> GetExchangeByIdAsync(string id)
         {
             var exc = await _unitOfWork.GetRepository<Exchange>().GetByIdAsync(id);
-            if (exc == null) throw new Exception("Exchange not found");
+            if (exc == null)
+            {
+                throw new ErrorException(StatusCodes.Status404NotFound, ResponseCodeConstants.NOT_FOUND, "Không tìm thấy trao đổi!!!");
+            }
+            var exchange = new ExchangeModelViews
+            {
+                BuyerId = exc.BuyerId.Value.ToString(),
+                ProductId = exc.ProductId
+            };
 
-            exc.DeletedBy = userId;
-            exc.DeletedTime = DateTime.Now;
+            return exchange ??
+                 throw new ErrorException(StatusCodes.Status404NotFound, ResponseCodeConstants.NOT_FOUND, "Không tìm thấy Trao đổi"); ;
 
-            _unitOfWork.GetRepository<Exchange>().UpdateAsync(exc);
-            await _unitOfWork.SaveAsync();
-            return exc;
-        }
-
-        public async Task<IEnumerable<Exchange>> GetAllExchangeAsync()
-        {
-            return await _unitOfWork.GetRepository<Exchange>().GetAllAsync();
-        }
-
-        public async Task<Exchange?> GetExchangeByIdAsync(string id)
-        {
-            return await _unitOfWork.GetRepository<Exchange>().GetByIdAsync(id);
         }
 
         public Task<BasePaginatedList<Exchange>> GetExchangePaginatedAsync(int pageIndex, int pageSize)
@@ -48,13 +108,30 @@ namespace FUExchange.Services.Service
             throw new NotImplementedException();
         }
 
-        public async Task<Exchange> UpdateExchangeAsync(Exchange exc, string userId)
+        public Task UpdateExchangeAsync(ExchangeModelViews exc, string userId)
         {
-            exc.LastUpdatedBy = userId;
-            exc.LastUpdatedTime = DateTime.Now;
-            _unitOfWork.GetRepository<Exchange>().UpdateAsync(exc);
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateExchangeAsync(string id, ExchangeModelViews updateExchangeModel)
+        {
+            IHttpContextAccessor httpContext = new HttpContextAccessor();
+            var User = httpContext.HttpContext?.User;
+            Guid userID = new Guid(User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value);
+            var existingExchange = await _unitOfWork.GetRepository<Exchange>().GetByIdAsync(id);
+
+            if (existingExchange == null)
+            {
+                throw new ErrorException(StatusCodes.Status404NotFound, ResponseCodeConstants.NOT_FOUND, "Không tìm thấy Trao đổi");
+            }
+            if (updateExchangeModel == null)
+                throw new KeyNotFoundException("Không có dữ liệu hợp lệ");
+            existingExchange.ProductId = updateExchangeModel.ProductId;
+            existingExchange.BuyerId = Guid.Parse(updateExchangeModel.BuyerId);
+            existingExchange.LastUpdatedBy = userID.ToString();
+            existingExchange.LastUpdatedTime = DateTime.Now;
+            await _unitOfWork.GetRepository<Exchange>().UpdateAsync(existingExchange);
             await _unitOfWork.SaveAsync();
-            return exc;
         }
     }
 }
