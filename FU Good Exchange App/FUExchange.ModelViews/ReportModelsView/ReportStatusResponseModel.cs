5bbc8f5bb0dd691493a54ff4626b2f261b6ba594
tranphu112 <tranhuuphu1598@gmail.com>
2024-10-11 14:03:54 +0700

    Update ReportStatusResponseModel.cs

--- FU Good Exchange App/FUExchange.ModelViews/ReportModelsView/ReportStatusResponseModel.cs
+++ FU Good Exchange App/FUExchange.ModelViews/ReportModelsView/ReportStatusResponseModel.cs
@@ -8,11 +8,11 @@ namespace FUExchange.ModelViews.ReportModelViews
 {
     public class ReportStatusResponseModel
     {
-        public string ReportId { get; set; }
+        public string? ReportId { get; set; }
         public bool Status { get; set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public DateTime CreatedTime { get; set; }
-        public string LastUpdatedBy { get; set; }
+        public string? LastUpdatedBy { get; set; }
         public DateTime? LastUpdatedTime { get; set; }
     }
 }
