23ec7253025dbd3c008581b31996c6b9ae93e7c5
Lê Văn Thắng <levanthang.2001202241@gmail.com>
2024-11-14 15:32:07 +0700

    add razor page product

---
+++ FU Good Exchange App/FU_GoodExchange_Pages/Entity/Exchange.cs
@@ -0,0 +1,15 @@
+﻿using FU_GooodExchange_Pages;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FU_GooodExchange_Pages
+{
+    public class Exchange : BaseEntity
+    {
+        public string ProductId { get; set; } = string.Empty;
+        public Guid BuyerId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product? Product { get; set; }
+        [ForeignKey("BuyerId")]
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
---
+++ FU Good Exchange App/FU_GoodExchange_Pages/Entity/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FU_GooodExchange_Pages
+{
+    public class ProductImage : BaseEntity
+    {
+        public string ProductId { get; set; } = string.Empty;
+        [Required]
+        public string Image {  get; set; } = string.Empty;
+        public string? Description { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product? Product { get; set; }
+
+    }
+}
