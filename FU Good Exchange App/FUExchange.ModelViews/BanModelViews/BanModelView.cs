7734e2efae86026b64adddf71930b01eec0768d4
Lê Hồ Hoàng Linh <135522356+HoangLinhLe@users.noreply.github.com>
2025-01-01 16:32:30 +0700

    update
    update BanModelView

--- FU Good Exchange App/FUExchange.ModelViews/BanModelViews/BanModelView.cs
+++ FU Good Exchange App/FUExchange.ModelViews/BanModelViews/BanModelView.cs
@@ -4,11 +4,17 @@ namespace FUExchange.ModelViews.BanModelViews
     public class BanModelView
     {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         required public string? ReportId { get; set; }
 =======
         required public string ReportId { get; set; }
         
 >>>>>>> Stashed changes
         required public DateTime? Expires { get; set; }
+=======
+        public required string Id { get; set; }
+        public required string ReportId { get; set; }
+        public DateTime? Expires {  get; set; } 
+>>>>>>> Stashed changes
     }
 }
